3ca1a3a21c305118eff803f2af9b5e7668718bad
Grace Conrad <g-conrad@onu.edu>
2023-10-26 13:49:32 -0400

    Completed 9.2

--- Lab 9.2/Source.cpp
+++ Lab 9.2/Source.cpp
@@ -57,3 +57,67 @@ bool hasDuplicates(int values[], int size)
     return false;
 
 }
+
+int main()
+{
+    const int arraySize = 6;
+    int values[arraySize];
+
+    // Input loop to acquire testing data
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << "Enter integer #" << i << ": "; cin >> values[i];
+
+    }
+
+    // Test and display results
+
+    if (isSortedIncreasing(values, arraySize))
+    {
+        cout << "The data are increasing." << endl;
+
+    }
+
+    else
+    {
+        cout << "The data are not increasing." << endl;
+
+    }
+
+    if (isSortedDecreasing(values, arraySize))
+    {
+        cout << "The data are decreasing." << endl;
+
+    }
+
+    else
+    {
+        cout << "The data are not decreasing." << endl;
+
+    }
+
+    if (hasAdjacentDuplicates(values, arraySize))
+    {
+        cout << "The data has adjacent duplicates." << endl;
+
+    }
+
+    else
+    {
+        cout << "The data does not have adjacent duplicates." << endl;
+
+    }
+
+    if (hasDuplicates(values, arraySize))
+    {
+        cout << "The data has duplicates." << endl;
+    }
+
+    else
+    {
+        cout << "The data does not have duplicates." << endl;
+    }
+
+    return 0;
+}
